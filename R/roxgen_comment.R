9963a428eba65572751467de36a5f62826db969b
Siying H <siying.huang-ext@sanofi.com>
2023-01-27 11:51:41 +0100

    :sparkles: + uncomment roxy re #9

--- R/roxgen_comment.R
+++ R/roxgen_comment.R
@@ -30,6 +30,23 @@ roxygen_quote <- function(txt) {
   stringr::str_c("#' > ", txt_split, collapse = "\n")
 }
 
+
+#' Undo roxygen comment
+#'
+#' @param text a string vector with roxygen comment sign.
+#'
+#' @return a string vector without roxygen comment
+#' @export
+redo_roxycomment <- function(text) {
+  stringr::str_split(text, "\n") %>%
+    unlist() %>%
+    stringr::str_extract_all("(?<=#').+") %>%
+    unlist() %>%
+    stringr::str_trim("left") %>%
+    stringr::str_c(collapse = "\n")
+}
+
+
 #' Roxygen comment on currently selected section
 #'
 #' @return
@@ -66,6 +83,24 @@ selection2roxygen_quote <- function(){
   if (nchar(selected) > 0) {
     message(sprintf("You have selected:\n %s",selected))
     output <- roxygen_quote(selected)
+    message(output)
+  } else {
+    message("Nothing selected.")
+    output <- ""
+  }
+  print(select_pos)
+  rstudioapi::insertText(select_pos, output, id)
+}
+
+
+uncomment_roxygen <-  function(){
+  doc <- rstudioapi::getActiveDocumentContext()
+  id <- doc$id
+  selected <- doc$selection[[1]]$text
+  select_pos <- doc$selection[[1]]$range
+  if (nchar(selected) > 0) {
+    message(sprintf("You have selected:\n %s",selected))
+    output <- redo_roxycomment(selected)
     message(output)
   } else {
     message("Nothing selected.")
