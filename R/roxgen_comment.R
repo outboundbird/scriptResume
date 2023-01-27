3b401877d8ff199c5fe407cafe49406cca84cd6e
Siying H <siying.huang-ext@sanofi.com>
2023-01-27 11:27:57 +0100

    :sparkles: + roxygen quote re #10

--- R/roxgen_comment.R
+++ R/roxgen_comment.R
@@ -16,6 +16,20 @@ convert_roxygen <- function(txt) {
   stringr::str_c("#' ", txt_split, collapse = "\n")
 }
 
+
+#' add roxygen comment with quotation sign.
+#'
+#' @param txt a string vector.
+#'
+#' @return quoted roxygen comments
+#' @export
+#' @examples
+#' # ADD_EXAMPLES_HERE
+roxygen_quote <- function(txt) {
+  txt_split <- stringr::str_split(txt, "\\\n")[[1]]
+  stringr::str_c("#' > ", txt_split, collapse = "\n")
+}
+
 #' Roxygen comment on currently selected section
 #'
 #' @return
@@ -30,6 +44,28 @@ selection2roxygen <- function(){
   if (nchar(selected) > 0) {
     message(sprintf("You have selected:\n %s",selected))
     output <- convert_roxygen(selected)
+    message(output)
+  } else {
+    message("Nothing selected.")
+    output <- ""
+  }
+  print(select_pos)
+  rstudioapi::insertText(select_pos, output, id)
+}
+
+
+#' Transform current text selection into roxygen comment with quotation.
+#'
+#' @return quoted roxygen text
+#' @export
+selection2roxygen_quote <- function(){
+  doc <- rstudioapi::getActiveDocumentContext()
+  id <- doc$id
+  selected <- doc$selection[[1]]$text
+  select_pos <- doc$selection[[1]]$range
+  if (nchar(selected) > 0) {
+    message(sprintf("You have selected:\n %s",selected))
+    output <- roxygen_quote(selected)
     message(output)
   } else {
     message("Nothing selected.")
