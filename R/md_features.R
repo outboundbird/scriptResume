0b562f4758552ff65c198102f267fc8dd217ff1e
Siying H <siying.huang-ext@sanofi.com>
2023-04-07 18:27:01 +0200

    re #17

--- R/md_features.R
+++ R/md_features.R
@@ -17,3 +17,45 @@ md_bullet <- function() {
   print(select_pos)
   rstudioapi::insertText(select_pos, output, id)
 }
+
+
+#' Pad the begining and end of a vector
+#' @param txt string vector with length of one.
+#' @param pattern pattern to pad.
+#'
+#' @return padded string
+padding <- function(txt, pattern) {
+  lapply(txt, function(x) glue::glue(pattern, x, pattern)) %>%
+    unlist()
+}
+
+
+#' Transform code to markdown table
+#' cells must be separated by colons `:`
+#' @return markdown table format
+#' @export
+md_tab <- function() {
+  doc <- rstudioapi::getActiveDocumentContext()
+  id <- doc$id
+  selected <- doc$selection[[1]]$text
+  select_pos <- doc$selection[[1]]$range
+  if (nchar(selected) > 0) {
+    message(sprintf("You have selected:\n %s", selected))
+    subunit <- str_split(selected, "\\\n") %>%
+      unlist() %>%
+      str_replace_all(":", "\\|") %>%
+      padding("|")
+
+    n_bar <- str_count(subunit[1], "\\|")
+    tab_edge <- paste(rep("|", n_bar), collapse = " --- ")
+
+    output <- append(subunit, tab_edge, after = 1) %>%
+      paste(collapse = "\n")
+    message(output)
+  } else {
+    message("Nothing selected.")
+    output <- ""
+  }
+  print(select_pos)
+  rstudioapi::insertText(select_pos, output, id)
+}
