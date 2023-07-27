36d82ac702e5ed1b35474cd4278e94ac3e9d3661
Siying H <siying.huang-ext@sanofi.com>
2023-07-27 14:46:10 +0200

    re :bug: #34 solved

--- R/create_css.R
+++ R/create_css.R
@@ -1,22 +1,21 @@
 #' Create a css file in the current directory
-#'
-#' @param css_content content of css file.
-#'
 #' @return Null. Create a .css file in the current directory
 #' @export
 create_css <- function() {
   doc <- rstudioapi::getActiveDocumentContext()
   current_f_path <- doc$path
   current_dir <- dirname(current_f_path)
-  message(paste("Creating css file in", current_dir))
+  message(paste("Creating css file in", current_dir, "/"))
   css_path <- system.file("extdata", "theme.css",
     package = "scriptResume",
     mustWork = T
   )
-  system2("cp",
-    args = c(
-      css_path,
-      paste(current_dir, "sanofi.css", sep = "/")
-    )
+  tryCatch(
+    {
+      file.copy(css_path, current_dir)
+    },
+    error = function(e) {
+      warning("File cannot be copied!")
+    }
   )
 }
