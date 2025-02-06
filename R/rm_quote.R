58a55cf265b724db44d77ed2500f44950b496721
outboundbird <syhuanguoft@gmail.com>
2025-02-06 11:37:26 +0000

    re remove quote marks #42

---
+++ R/rm_quote.R
@@ -0,0 +1,25 @@
+#' Remove Quotes from Strings
+#'
+#' This function removes all quotation marks from the input strings.
+#'
+#' @return A character vector with all quotation marks removed.
+#' @examples
+#' rm_quotes("Hello, \"world\"!")
+#' # [1] "Hello, world!"
+#' @export
+rm_quotes <- function() {
+  doc <- rstudioapi::getActiveDocumentContext()
+  id <- doc$id
+  selected <- doc$selection[[1]]$text
+  select_pos <- doc$selection[[1]]$range
+  if (nchar(selected) > 0) {
+    message(sprintf("You have selected:\n %s", selected))
+    output <- gsub('"', "", text)
+    message(output)
+  } else {
+    message("Nothing selected.")
+    output <- ""
+  }
+  print(select_pos)
+  rstudioapi::insertText(select_pos, output, id)
+}
