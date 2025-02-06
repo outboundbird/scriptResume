35524a1cf8ba44e795af4103dd4ad119a21cafca
outboundbird <syhuanguoft@gmail.com>
2025-02-06 11:26:43 +0000

    wip add source triplet env setup #45

---
+++ src/scrapples.R
@@ -0,0 +1,24 @@
+library(rstudioapi)
+library(stringr)
+documentPath()
+getActiveDocumentContext()
+path <- getActiveDocumentContext()$path
+
+path <- "~/wise/DEVOPS/SAR447537/INBRX101/BMK/EXPLO/PGM/datacheck_BMK.R"
+
+triplet <- str_split(path, "/") %>% 
+unlist() %>% 
+.[1:6] %>% 
+paste(., collapse = "/")
+
+source_loc <- "METADATA/WISE/alloc_ana.R"
+
+triplet_source_path <- file.path(triplet, source_loc)
+
+
+source_cmd <- paste0("source(",triplet_source_path,")")
+rstudioapi::callFun(source_cmd)
+
+rstudioapi::callFun("versionInfo")
+
+source_triplet_env()
